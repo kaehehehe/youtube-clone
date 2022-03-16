1d0c13dce479ec8a5248e45a9e464ae194653714
Kae <kaehehehe@gmail.com>
2022-03-16 11:51:49 +0900

    :sparkles: router 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import Video from './pages/Video/Video';
 
 const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:id" element={<Video />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/Video/Video.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VIdeo = () => {
+  return (
+    <div>
+      video page
+    </div>
+  );
+};
+
+export default VIdeo;
