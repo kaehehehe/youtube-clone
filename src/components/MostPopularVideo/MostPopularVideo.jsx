06b464176672b1233eefa5802a52e4aded6b5c77
Kae <kaehehehe@gmail.com>
2022-03-17 14:47:42 +0900

    :sparkles: 동영상을 클릭 시 해당 동영상 상세페이지로 이동 및 자동 재생이 되도록 구현

--- src/components/MostPopularVideo/MostPopularVideo.jsx
+++ src/components/MostPopularVideo/MostPopularVideo.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useWindowSize } from 'react-use';
 import { VscDebugStackframeDot } from 'react-icons/vsc';
+import { useNavigate } from 'react-router-dom';
 
 import * as S from './style';
 import { convertDataIntoAgo } from '../../utils/convertDataIntoAgo';
@@ -11,43 +12,47 @@ const MostPopularVideo = ({ video }) => {
   const heightRef = useRef(null);
   const { width, height } = useWindowSize(null);
   const [thumbnailHeight, setThumbnailHeight] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const thumbnailHeight = heightRef.current.height;
     setThumbnailHeight(thumbnailHeight);
   }, [[width, height]]);
 
+  const handleClickVideo = () => {
+    const { id } = video;
+    navigate(`/video/${id}`);
+  };
+
   return (
-    <>
-      <S.Video>
-        <S.ThumbnailWrapper>
-          <S.Thumbnail
-            src={video.snippet.thumbnails.medium.url}
-            alt="video thumbnail"
-            ref={heightRef}
-          />
-          <S.Duration height={thumbnailHeight}>
-            {convertDataIntoMinutesAndSeconds(video.contentDetails.duration)}
-          </S.Duration>
-        </S.ThumbnailWrapper>
-        <S.Wrapper>
-          {/* <S.ChannelThumbnail src={} alt="channel thumbnail" /> */}
-          <S.VideoMetadata>
-            <p className="title">{video.snippet.title}</p>
-            <p className="channel-name">{video.snippet.channelTitle}</p>
-            <S.MetadataWrapper>
-              <span>{`${convertDataIntoNumberUsingUnits(
-                video.statistics.viewCount
-              )} views`}</span>
-              <VscDebugStackframeDot color="gray" />
-              <span className="published-at">
-                {convertDataIntoAgo(video.snippet.publishedAt)} ago
-              </span>
-            </S.MetadataWrapper>
-          </S.VideoMetadata>
-        </S.Wrapper>
-      </S.Video>
-    </>
+    <S.Video onClick={handleClickVideo}>
+      <S.ThumbnailWrapper>
+        <S.Thumbnail
+          src={video.snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+          ref={heightRef}
+        />
+        <S.Duration height={thumbnailHeight}>
+          {convertDataIntoMinutesAndSeconds(video.contentDetails.duration)}
+        </S.Duration>
+      </S.ThumbnailWrapper>
+      <S.Wrapper>
+        {/* <S.ChannelThumbnail src={} alt="channel thumbnail" /> */}
+        <S.VideoMetadata>
+          <p className="title">{video.snippet.title}</p>
+          <p className="channel-name">{video.snippet.channelTitle}</p>
+          <S.MetadataWrapper>
+            <span>{`${convertDataIntoNumberUsingUnits(
+              video.statistics.viewCount
+            )} views`}</span>
+            <VscDebugStackframeDot color="gray" />
+            <span className="published-at">
+              {convertDataIntoAgo(video.snippet.publishedAt)} ago
+            </span>
+          </S.MetadataWrapper>
+        </S.VideoMetadata>
+      </S.Wrapper>
+    </S.Video>
   );
 };
 
--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,7 +1,107 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  MdThumbUpAlt,
+  MdThumbDownAlt,
+  MdShare,
+  MdFileDownload,
+  MdBookmarkAdd,
+  MdMoreHoriz,
+} from 'react-icons/md';
+import { VscDebugStackframeDot } from 'react-icons/vsc';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { convertDataIntoMonthDayYear } from '../../utils/convertDataIntoMonthDayYear';
+import { convertDataIntoNumberUsingUnits } from '../../utils/convertDataIntoNumberUsingUnits';
+import * as S from './style';
 
 const Video = () => {
-  return <div>video page</div>;
+  const { videos } = useSelector((state) => state.mostPopularVideos);
+  const [video, setVideo] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const video = videos.filter((video) => video.id === id);
+    setVideo(video[0]);
+  }, []);
+  return (
+    <>
+      {video && (
+        <S.Container>
+          <S.Video
+            type="text/html"
+            src={`https://www.youtube.com/embed/${video.id}?autoplay=1`}
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay"
+          ></S.Video>
+          <S.Wrapper>
+            <S.VideoTitle>{video.snippet.title}</S.VideoTitle>
+            <S.Actions>
+              <S.Metadata>
+                <span>{`${Number(
+                  video.statistics.viewCount
+                ).toLocaleString()} views`}</span>
+                <VscDebugStackframeDot color="gray" />
+                <span>
+                  {convertDataIntoMonthDayYear(video.snippet.publishedAt)}
+                </span>
+              </S.Metadata>
+              <S.Buttons>
+                <S.Button>
+                  <MdThumbUpAlt />
+                  <S.ButtonName>
+                    {convertDataIntoNumberUsingUnits(
+                      video.statistics.likeCount
+                    )}
+                  </S.ButtonName>
+                </S.Button>
+                <S.Button>
+                  <MdThumbDownAlt />
+                  <S.ButtonName>DISLIKE</S.ButtonName>
+                </S.Button>
+                <S.Button>
+                  <MdShare />
+                  <S.ButtonName>SHARE</S.ButtonName>
+                </S.Button>
+                <S.Button>
+                  <MdFileDownload />
+                  <S.ButtonName>DOWNLOAD</S.ButtonName>
+                </S.Button>
+                <S.Button>
+                  <MdBookmarkAdd />
+                  <S.ButtonName>SAVE</S.ButtonName>
+                </S.Button>
+                <S.Button>
+                  <MdMoreHoriz />
+                </S.Button>
+              </S.Buttons>
+            </S.Actions>
+            <S.VideoInfo>
+              <S.VideoInfoWrapper>
+                <S.ChannelIconWrapper>
+                  {/* <S.ChannelIcon src={channel} alt="channel thumbnail" /> */}
+                  <S.ChannelTitleWrapper>
+                    <S.ChannelTitle>
+                      {video.snippet.channelTitle}
+                    </S.ChannelTitle>
+                    {/* <S.Subscribers>{`${convertDataIntoNumberUsingUnits(
+                subscriberCount
+              )} subscribers`}</S.Subscribers> */}
+                  </S.ChannelTitleWrapper>
+                </S.ChannelIconWrapper>
+                <S.SubBtn>subscribe</S.SubBtn>
+              </S.VideoInfoWrapper>
+              <S.VideoDescription>
+                {video.snippet.description}
+              </S.VideoDescription>
+            </S.VideoInfo>
+          </S.Wrapper>
+          {/* Comment */}
+        </S.Container>
+      )}
+    </>
+  );
 };
 
 export default Video;
