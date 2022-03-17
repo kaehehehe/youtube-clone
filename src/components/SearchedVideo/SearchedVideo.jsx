4a24052f63ea8e10626f5260a28fe05963390984
Kae <kaehehehe@gmail.com>
2022-03-17 17:35:51 +0900

    :sparkles: 채널 썸네일 추가

--- src/components/SearchedVideo/SearchedVideo.jsx
+++ src/components/SearchedVideo/SearchedVideo.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import * as S from './style';
 import { convertDataIntoAgo } from '../../utils/convertDataIntoAgo';
 
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
 const SearchedVideo = ({ video }) => {
+  const [channel, setChannel] = useState();
+
+  const fetchChannelData = async (channelId) => {
+    return await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${KEY}`,
+      { method: 'GET', redirect: 'follow' }
+    );
+  };
+
+  useEffect(() => {
+    fetchChannelData(video.snippet.channelId).then((res) =>
+      setChannel(res.data.items[0])
+    );
+  }, []);
   return (
-    <S.Video>
-      <S.Thumbnail>
-        <img src={video.snippet.thumbnails.medium.url} alt="video thumbnail" />
-      </S.Thumbnail>
-      <S.VideoMetadata>
-        <S.Title>{video.snippet.title}</S.Title>
-        <S.PublishedAt>
-          {convertDataIntoAgo(video.snippet.publishedAt)} ago
-        </S.PublishedAt>
-        <S.Wrapper>
-          {/* <S.ChannelImg src={channel} alt="channel thumbnail" /> */}
-          <S.Channel>{video.snippet.channelTitle}</S.Channel>
-        </S.Wrapper>
-        <S.Description>{video.snippet.description}</S.Description>
-      </S.VideoMetadata>
-    </S.Video>
+    <>
+      {channel && (
+        <S.Video>
+          <S.Thumbnail>
+            <img
+              src={video.snippet.thumbnails.medium.url}
+              alt="video thumbnail"
+            />
+          </S.Thumbnail>
+          <S.VideoMetadata>
+            <S.Title>{video.snippet.title}</S.Title>
+            <S.PublishedAt>
+              {convertDataIntoAgo(video.snippet.publishedAt)} ago
+            </S.PublishedAt>
+            <S.Wrapper>
+              <S.ChannelImg
+                src={channel.snippet.thumbnails.default.url}
+                alt="channel thumbnail"
+              />
+              <S.Channel>{video.snippet.channelTitle}</S.Channel>
+            </S.Wrapper>
+            <S.Description>{video.snippet.description}</S.Description>
+          </S.VideoMetadata>
+        </S.Video>
+      )}
+    </>
   );
 };
 
