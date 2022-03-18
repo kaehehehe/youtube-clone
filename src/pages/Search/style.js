aa03fbace7c669d8f9d30246ccaa1a3e73aeb80a
Kae <kaehehehe@gmail.com>
2022-03-18 16:06:45 +0900

    :lipstick: 불필요한 margin 제거

--- src/pages/Search/style.js
+++ src/pages/Search/style.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.ul`
   background-color: var(--dark-black-color);
   text-align: start;
-  margin-top: 70px;
   margin-left: 10px;
   margin-right: 30px;
   display: flex;
--- src/pages/Video/style.js
+++ src/pages/Video/style.js
@@ -11,7 +11,6 @@ export const Container = styled.div`
 `;
 
 export const Video = styled.iframe`
-  margin-top: 70px;
   width: 100%;
   height: 500px;
 `;
